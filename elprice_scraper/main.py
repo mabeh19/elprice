274ff776a871d0bed7e3ffb50908eab9ea68748f
mabeh19 <mathiasibehrens@gmail.com>
2023-02-04 10:49:16 +0100

    Added wait until next hour

--- elprice_scraper/main.py
+++ elprice_scraper/main.py
@@ -1,11 +1,20 @@
 import time
+import datetime
 import socket
 import scraper
 
 ONE_MINUTE = 60
 ONE_HOUR = 60 * 60
+NEXT_HOUR = 1
 IP_ADDRESS = "10.0.2.15"
 PORT = 35000
+
+def sleepUntil(hour):
+    t = datetime.datetime.today()
+    future = datetime.datetime(t.year, t.month, t.day, t.hour + hour, 0)
+    if t.timestamp() > future.timestamp():
+        future += datetime.timedelta(days=1)
+    time.sleep((future-t).total_seconds())
 
 def upload_to_server(price):
     print("Creating socket...")
@@ -16,6 +25,9 @@ def upload_to_server(price):
     print("Data sent, closing connection...")
     s.close()
 
+print("Waiting for the beginning of next hour")
+sleepUntil(NEXT_HOUR)
+
 while True:
     try: 
         current_price = scraper.get_current_price()
@@ -23,8 +35,8 @@ while True:
         upload_to_server(current_price)
 
         print("Success! Going to sleep...")
-
-        time.sleep(ONE_HOUR)
+    
+        sleepUntil(NEXT_HOUR)
     except:
         print("Error occurred retrying in a minute")
         time.sleep(ONE_MINUTE)
